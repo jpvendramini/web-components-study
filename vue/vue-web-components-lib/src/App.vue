afb8b0579a8db00238b587dfb7218d1d9429abda
João Pedro Vendramini <joaopedrovendraminiv90@gmail.com>
2023-10-18 23:49:22 -0300

    feat: vue version

--- vue/vue-web-components-lib/src/App.vue
+++ vue/vue-web-components-lib/src/App.vue
@@ -1,47 +1,126 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container">
+    <title-component text="Most recent books" />
+    <label-component text="Here you can visualize the most recent added books." />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="books-container">
+      <book-item-component
+        v-for="book in recentBooks.slice(0, 5)"
+        :key="book.id"
+        :title="book.title"
+        :label="book.authors"
+        :path="book.image"
+        @click="getBookDetails(book.id)"
+      />
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="search-container">
+      <div>
+        <title-component text="Search for books" />
+        <label-component
+          text="Here you can search your book of interest by the name of the author or book title."
+        />
+      </div>
+      <div>
+        <search-input-component ref="searchInputRef" :value="searchQuery" @input="handleSearchInputChange" />
+      </div>
+    </div>
+
+    <div class="books-container">
+      <book-item-component
+        v-for="book in searchedBooks"
+        :key="book.id"
+        :title="book.title"
+        :label="book.authors"
+        :path="book.image"
+        @click="getBookDetails(book.id)"
+      />
+    </div>
+
+    <sidebar-component :bookInfo="bookInfo" />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+export default {
+  data() {
+    return {
+      recentBooks: [],
+      searchedBooks: [],
+      bookInfo: null,
+      searchQuery: '',
+      currentBookId: '',
+    };
+  },
+  mounted() {
+    this.fetchData();
+    this.fetchSearchedData("programming");
+    const searchInput = this.$refs.searchInputRef;
+    searchInput.addEventListener("change", this.handleSearchInputChange);
+  },
+  methods: {
+    fetchData() {
+      fetch("https://www.dbooks.org/api/recent")
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          if (data.status === "ok" && data.books && data.books.length > 0) {
+            this.recentBooks = data.books;
+          }
+        });
+    },
+    getBookDetails(bookId) {
+      if (this.currentBookId !== bookId) {
+        fetch("https://www.dbooks.org/api/book/" + bookId)
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Network response was not ok");
+            }
+            return response.json();
+          })
+          .then((data) => {
+            this.bookInfo = data;
+            this.currentBookId = data.id;
+          });
+      } else {
+        this.bookInfo = null;
+        this.currentBookId = '';
+      }
+    },
+    fetchSearchedData(query) {
+      if (query !== "") {
+        fetch("https://www.dbooks.org/api/search/" + query)
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Network response was not ok");
+            }
+            return response.json();
+          })
+          .then((data) => {
+            if (data.status === "ok" && data.books && data.books.length > 0) {
+              this.searchedBooks = data.books;
+            }
+          });
+      }
+    },
+    handleSearchInputChange(event) {
+      this.searchQuery = event.target.value;
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+      console.log(event.target.value);
+      // Clear previous timer, if it exists
+      if (this.searchInputTimer) {
+        clearTimeout(this.searchInputTimer);
+      }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+      // Set a new timer to fetch data after a delay
+      this.searchInputTimer = setTimeout(() => {
+        this.fetchSearchedData(event.target.value);
+      }, 1000); // 1000 milliseconds (1 second)
+    },
+  },
+};
+</script>
--- vue/vue-web-components-lib/vite.config.js
+++ vue/vue-web-components-lib/vite.config.js
@@ -1,16 +1,31 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+const customComponents = [
+  "search-input-component",
+  "label-component",
+  "title-component",
+  "sidebar-component",
+  "book-item-component",
+];
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          // treat all tags with a dash as custom elements
+          isCustomElement: (tag) => customComponents.includes(tag),
+        },
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
